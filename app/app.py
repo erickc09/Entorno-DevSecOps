920f83585edfe6afef99342e000dc0cd535cdad8
erickc09 <57420598+erickc09@users.noreply.github.com>
2024-11-27 21:46:44 -0500

    a

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 
+# Crear instancia de Flask
 app = Flask(__name__)
-app.config['ENV'] = 'development'  # Asegurar entorno de desarrollo
-app.config['DEBUG'] = True  # Activar depuración
+
+# Configuración del entorno
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Hacer que el objeto `app` esté disponible para pruebas
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
